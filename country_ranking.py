4ee69a74575007a0dd1313dd311a36803c67995b
AZPS <github@londonanalytics.info>
2024-02-21 09:35:42 +0000

    all quotas are within 1 of the official page, but it's not clear what's making the difference

--- country_ranking.py
+++ country_ranking.py
@@ -32,7 +32,7 @@ class CountryRankingEngine:
         rules = str(ruleset).replace("RulesetClass.", "")
 
         all_players = self.db.query(Player).filter(
-            Player.ema_id != -1).filter(Player.country_id != "??").filter(
+            Player.ema_id != -1).filter(
             getattr(Player, f"{rules}_rank") != None).order_by(
             getattr(Player, f"{rules}_rank").desc())
 
@@ -41,7 +41,8 @@ class CountryRankingEngine:
 
         self.player_count = len(all_players.all())
         self.players700plus = len(all_700plus.all())
-        for c in self.db.query(Country).filter(Country.id != "??"): # .filter(Country.ema_since is not None):
+        for c in self.db.query(Country).filter(Country.id != "??").filter(
+                Country.ema_since is not None):
             c.ema_since = None if c.id in ("ru", "by") else datetime.now()
             c_all = all_players.filter(Player.country_id == c.id)
             player_count = len(c_all.all())
@@ -50,10 +51,6 @@ class CountryRankingEngine:
                 )
             setattr(c, f"player_count_{rules}", player_count)
             setattr(c, f"over700_{rules}", c_700plus)
-            setattr(c, f"propn_of_all_players_700plus_{rules}",
-                    c_700plus / self.players700plus)
-            setattr(c, f"propn_of_all_ranked_players_{rules}",
-                    player_count / self.player_count)
             setattr(c, f"country_ranking_{rules}", None)
             if player_count > 0:
                 top3 = c_all.limit(3)
